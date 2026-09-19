1ceae9c1f194235abe46d953c542078cad58f8a4
Muhammad Zulfy <2410010331zulfy@gmail.com>
2026-09-19 11:12:07 +0800

    Menambahkan file Kartu Mahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author USer
+ */
+public class KartuMahasiswa {
+    public static void main(String[] args) {
+        String nama = "Muhammad Zulfy";
+        String npm = "2410010331";
+        String prog_studi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Game Developer";
+        
+        System.out.println("======================================");
+        System.out.println("  KARTU MAHASISWA PBO 2");
+        System.out.println("======================================");
+        
+        System.out.println("  Nama      : " + nama);
+        System.out.println("  NPM       : " + npm);
+        System.out.println("  Prodi     : " + prog_studi);
+        System.out.println("  Semester  : " + semester);
+        System.out.println("  Alasan    : " + alasan);
+        System.out.println("======================================");
+    }
+}
