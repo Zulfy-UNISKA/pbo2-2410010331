7c916a002b50cc5810df3aa6c1f745d9a7f70924
Muhammad Zulfy <2410010331zulfy@gmail.com>
2026-09-26 20:29:05 +0800

    P02: studi kasus Perpustakaan Mini

---
+++ p02-perpustakaan-mini/src/main/java/id/ac/uniska/pbo2/p02/StatusKoleksi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package id.ac.uniska.pbo2.p02;
+
+/**
+ *
+ * @author USer
+ */
+/**
+ * Status sebuah koleksi perpustakaan. Enum membatasi nilai hanya pada pilihan yang sah.
+*/
+public enum StatusKoleksi {
+    TERSEDIA,
+    DIPINJAM
+}
